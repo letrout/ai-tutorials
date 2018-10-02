4c8eeff5bb642cf11419e266405f4e193f3146b9
Joel Luth <33353303+letrout@users.noreply.github.com>
2018-10-01 23:41:13 -0500

    Matmult control some parameters via passed arguments (#3)

--- perf-matmult/perf-matmult.py
+++ perf-matmult/perf-matmult.py
@@ -5,6 +5,7 @@ Based on https://hackernoon.com/introduction-of-tensorflow-with-python-f4a9624f2
 """
 
 from __future__ import print_function
+import argparse
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
@@ -19,16 +20,11 @@ MATRIX_STEP = 50
 # Put a leash on tf logging to console
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
 
-def get_times(maximum_time):
+def get_times(max_time, matrix_sizes, device_names):
     rows = list()
-    device_names = ["/cpu:0"]
-    if tf.test.is_gpu_available():
-        device_names.append("/gpu:0")
-    matrix_sizes = range(MATRIX_MIN,MATRIX_MAX,MATRIX_STEP)
-
-    max_time = False
+    max_hit = False
     for size in matrix_sizes:
-        if max_time:
+        if max_hit:
             break
         for device_name in device_names:
 
@@ -39,30 +35,120 @@ def get_times(maximum_time):
                 r2 = tf.random_uniform(shape=shape, minval=0, maxval=1, dtype=data_type)
                 dot_operation = tf.matmul(r2, r1)
 
-            print("####### Calculating on the " + device_name + " #######")
-            with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
-                start_time = time.time()
-                result = session.run(dot_operation)
-                time_taken = time.time() - start_time
-                #print(result)
-                rows.append({'matrix': size, device_name: time_taken})
+            print("####### Calculating on the {}, matrix size {} #######".format(device_name, size))
+            try:
+                with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as session:
+                    start_time = time.time()
+                    result = session.run(dot_operation)
+                    time_taken = time.time() - start_time
+                    #print(result)
+                    rows.append({'matrix': size, device_name: time_taken})
+            except tf.errors.InternalError as e:
+                # eg tensorflow.python.framework.errors_impl.InternalError: Failed to create session
+                # run out of GPU memory?
+                # still sometimes crashes with
+                # tensorflow/core/common_runtime/gpu/gpu_event_mgr.cc:206] Unexpected Event status: 1
+                print("hit exception: ", e)
+                print("Matrix size attempted: ", size)
+                max_hit = True
+                break
 
-            if time_taken > maximum_time:
-                max_time = True
+            if time_taken > max_time:
+                max_hit = True
                 break
     columns = list(['matrix'])
     columns.extend(device_names)
     return pd.DataFrame(rows, columns=columns)
 
-df = get_times(MAX_ITER_SEC)
+def parse_arguments():
+    """
+    Parse script arguments
+    :return: argparse parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--max_time',
+        dest='max_time',
+        help="""Max run time in seconds (default={}""".format(MAX_ITER_SEC),
+        type=int,
+        default=MAX_ITER_SEC
+    )
+    parser.add_argument(
+        '--matrix_min',
+        dest='matrix_min',
+        help="""Minimum matrix size (default={}""".format(MATRIX_MIN),
+        type=int,
+        default=MATRIX_MIN
+    )
+    parser.add_argument(
+        '--matrix_max',
+        dest='matrix_max',
+        help="""Maximum matrix size (default={}""".format(MATRIX_MAX),
+        type=int,
+        default=MATRIX_MAX
+    )
+    parser.add_argument(
+        '--matrix_step',
+        dest='matrix_step',
+        help="""Matrix step size (default={}""".format(MATRIX_STEP),
+        type=int,
+        default=MATRIX_STEP
+    )
+    parser.add_argument(
+        '--no_cpu',
+        dest='no_cpu',
+        help='Do not run on CPU (default=False)',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--no_gpu',
+        dest='no_gpu',
+        help='Do not run on GPU (default=False)',
+        action='store_true',
+    )
+    parser.add_argument(
+        '-d',
+        '--directory',
+        dest='directory',
+        help='The directory in which to write csv file (default=None)',
+        type=str,
+        default=None
+    )
+    parser.add_argument(
+        '-p',
+        '--plot',
+        dest='plot',
+        help='Plot the test runs',
+        action='store_true',
+    )
+    return parser.parse_args()
 
-matrix_sizes = df['matrix'].tolist()
-for column in df.columns.tolist():
-    if column == 'matrix':
-        continue
-    times = df[column].tolist()
-    plt.plot(matrix_sizes, times, 'o-', label=column.strip('/'))
-plt.ylabel('Time')
-plt.xlabel('Matrix size')
-plt.legend()
-plt.show()
+def plot_runs(df):
+    matrix_sizes = df['matrix'].tolist()
+    for column in df.columns.tolist():
+        if column == 'matrix':
+            continue
+        times = df[column].tolist()
+        plt.plot(matrix_sizes, times, 'o-', label=column.strip('/'))
+    plt.ylabel('Time')
+    plt.xlabel('Matrix size')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    matrix_sizes = range(args.matrix_min, args.matrix_max, args.matrix_step)
+    device_names = list()
+    if not args.no_cpu:
+        device_names.append("/cpu:0")
+    if not args.no_gpu and tf.test.is_gpu_available():
+        device_names.append("/gpu:0")
+    df = get_times(args.max_time, matrix_sizes, device_names)
+    print(df)
+    if args.directory:
+        try:
+            df.to_csv(args.directory + '/matmult.csv')
+        except FileNotFoundError:
+            print("Unable to write to directory: ", args.directory)
+    if args.plot:
+        plot_runs(df)
