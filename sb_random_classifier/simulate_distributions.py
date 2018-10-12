bc435bda785cb2f8c88cea4d4200d54331044412
letrout <33353303+letrout@users.noreply.github.com>
2018-10-11 21:37:46 -0500

    distribution generator
    this will (eventually) be used to generate a dataset of random distribution classifications that simulate what I see with sysbench memory tests

---
+++ sb_random_classifier/simulate_distributions.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+"""
+Simulate the test result distributions we see from repeated runs of the
+sysbench memory benchmark. Qualitative classifications of these distributions:
+1. "clean": Normal/Gaussian distribution with <1% stddev/mean
+2. "fuzz": Normal/Gaussian distribution with >5% stddev/mean
+3. "peaks": "clean" with occasional peaks of x * mean (x>1), occasional <<50%
+4. "dips": "clean" with occasional drops of y * mean (0<y<1), occasional <<50%
+5(?) "bimodal": "clean with occasional peaks/dips, where occasional is ~50%
+
+Intended for use in training a xNN model to classify sysbench mem results
+by their amount and type of randomness
+
+Observed:
+clean: cv of <0.5%
+fuzz: cv up to 40%
+peaks of 10-40% freq, 1.5-1.6x baseline
+dips of 5-10% freq, 0.5-0.75x baseline
+We should generate a training dataset that goes somewhat outside these
+observations to avoid overfitting
+
+In this script I use "cv" as shorthand for coefficient of variation,
+or stddev/mean
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+DEFAULT_SAMPLES = 96
+CLEAN_MEANS = [100]
+CLEAN_CVS = [0.001, 0.003, 0.005, 0.01, 0.02]
+FUZZ_MEANS = CLEAN_MEANS
+FUZZ_CVS = [0.10, 0.20, 0.25, 0.30, 0.40]
+PEAK_BASE_PARAMS = {
+    'mean': 100,
+    'cv': 0.02
+}
+
+def create_normal_series(mean, cv, count=DEFAULT_SAMPLES):
+    """
+    Create a series of normal distribution
+    :param mean: mean of the distribution
+    :param cv: coefficient of variation of the distribution
+    :param count: length of the data series created
+    :return: Series with the specified distribution
+    """
+    return pd.Series(np.random.normal(mean, cv * mean, count))
+
+def create_normal_df(repeat=5, num_rows=DEFAULT_SAMPLES, means=[], cvs=[]):
+    df = pd.DataFrame()
+    j = -1
+    for i in range(0, repeat, 1):
+        for mean in means:
+            for cv in cvs:
+                j += 1
+                series = create_normal_series(mean, cv, num_rows)
+                df.insert(j, j, series)
+    return df
+
+def create_peak_series(peak_mean, peak_cv, peak_freq, count, series_base=None):
+    """
+    Create a 0-series with occasional peaks (or dips)
+    :param peak_mean: mean height of peaks (negative number for dips)
+    :param peak_cv: CV of the height of the peaks/dips
+    :param peak_freq: frequency (0-1) of the peaks/dips
+    :param count: total length of the series
+    :return: 0-series with occasional peaks or dips
+    """
+    # Create a "base" series on which we will apply our peaks
+    if series_base is None or count != len(series_base.index):
+        series_base = create_normal_series(
+            CLEAN_MEANS[0], CLEAN_CVS[0], count
+        )
+    peaks = lambda x: np.random.normal(peak_mean, peak_cv * peak_mean, 1)[0]\
+       if np.random.uniform() < peak_freq else 0
+    return series_base.apply(peaks)
+
+def create_peak_df(
+        repeat=5, num_rows=DEFAULT_SAMPLES, peak_means=[], peak_cvs=[], peak_freqs=[]):
+    df = pd.DataFrame()
+    j = -1
+    for i in range(0, repeat, 1):
+        for mean in peak_means:
+            for cv in peak_cvs:
+                for freq in peak_freqs:
+                    j += 1
+                    series = create_peak_series(mean, cv, freq, num_rows)
+                    df.insert(j, j, series)
+    return df
+
+def plot_series_hist(df, bins=20):
+    df.plot.hist(bins=bins)
+    plt.show()
+
+def plot_series_line(df):
+    df.plot.line()
+    plt.show()
+
+if __name__ == '__main__':
+    np.random.seed()
+    df_clean = create_normal_df(
+        repeat=5, num_rows=DEFAULT_SAMPLES, means=CLEAN_MEANS, cvs=CLEAN_CVS
+    )
+    df_fuzz = create_normal_df(
+        repeat=5, num_rows=DEFAULT_SAMPLES, means=FUZZ_MEANS, cvs=FUZZ_CVS
+    )
+    print(df_clean)
+    print(df_fuzz)
+    df_clean.shape
+    #peak_series = create_peak_series(40, 0.05, 0.1, 100)
+    #plot_series_hist(peak_series)
+    #plot_series_line(peak_series)
